d7c777b2b75dd0b73c3e7371338e239d65d507e3
Serhii <ex@example.com>
2022-08-05 11:30:58 +0300

    First commit, add model, controller, view(Post, Tag, Post)

---
+++ app/Http/Controllers/Admin/Post/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+}
